e1ba83722305290f45d17b0ab06a64fecb8ea05f
tienhg1310 <tienhg201@gmail.com>
2023-10-31 23:28:33 +0700

    feat/create_server_test_api_and script postman

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import sum from "./utils"
-import "./styles/style.css"
-import "./styles/style.scss"
-
-console.log(sum(100, 70))
--- src/utils.js
+++
@@ -1,3 +0,0 @@
-const sum = (num1, num2) => num1 + num2
-
-console.log(sum(100, 70))
