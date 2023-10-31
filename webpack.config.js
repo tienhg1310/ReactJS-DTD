e873bf16cde4e75fc65bc15ed4560493ddb78755
tien.nguyen9 <tien.nguyen9@ntq-solution.com.vn>
2023-10-31 17:51:11 +0700

    config html webpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,28 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     mode: "production",
     entry: {
         app: path.resolve("src/index.js"),
     },
-
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].js",
     },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss|css$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack",
+            filename: "index.html",
+            template: "src/template.html",
+        }),
+    ],
 }
