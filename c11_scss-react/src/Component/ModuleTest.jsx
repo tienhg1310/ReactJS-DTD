c249b6a210e98b12c4ce5b01090e96bf010fa9af
tien.nguyen9 <tien.nguyen9@ntq-solution.com.vn>
2023-11-03 10:41:21 +0700

    done c11

---
+++ c11_scss-react/src/Component/ModuleTest.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './ModuleTest.module.scss'
+
+const ModuleTest = () => {
+  return <div className={styles.container}>ModuleTest</div>
+}
+
+export default ModuleTest
